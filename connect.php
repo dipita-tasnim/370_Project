1f6a592b7f2da1c55785373c44a8234b7a0f7370
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-16 19:04:14 +0600

    random stuff

--- connect.php
+++ connect.php
@@ -1,14 +1,15 @@
 <?php
     $db_server = "localhost";
-    $db_name = "job_site";
+    $db_name = "jobsite";
     $db_password = "";
     $db_username = "root";
 
     $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
-    
-    if($conn) {
-        header("Location: homepage.html");
+
+    if(!$conn) {
+        die("Connection Failed: " . mysqli_connect_error());
     } else {
-        echo "Connection Failed";
+        header("Location: homepage.html");
+        exit();
     }
 ?>
