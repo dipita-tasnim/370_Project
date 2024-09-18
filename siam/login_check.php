2b23036a7f9bf60aac8b702421cdad2faff45115
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-18 23:04:06 +0600

    login finally works

--- siam/login_check.php
+++ siam/login_check.php
@@ -1,21 +1,16 @@
 <?php
     require_once("connect.php");
 
-
-    if("isset[$username]" && "isset[$password]"){
+    if(isset($_POST['username']) && isset($_POST['password'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
         $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($conn, $sql);
-    }
-    else{
-        echo ("Please enter username and password");
-    }
 
-    if($result->num_rows > 0){
-        header("Location: candidateORemployer.php");
-    }else{
-        echo ("Wrong username or password");
+        if(mysqli_num_rows($result) > 0){
+            header("Location: candidateORemployer.php");
+        }else{
+            echo 'Wrong username or password';
+        }
     }
-    
 ?>
