4a83d2ce79cd987d1e3488b14a8fae9520c0e150
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-18 21:19:25 +0600

    testing

--- siam/login_check.php
+++ siam/login_check.php
@@ -1,16 +1,21 @@
 <?php
     require_once("connect.php");
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+
     if("isset[$username]" && "isset[$password]"){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
         $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($conn, $sql);
+    }
+    else{
+        echo ("Please enter username and password");
+    }
 
     if($result->num_rows > 0){
         header("Location: candidateORemployer.php");
     }else{
-        die("Wrong username or password");
+        echo ("Wrong username or password");
     }
-    }
+    
 ?>
