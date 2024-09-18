b23ffaf0614d39f67f3e03e3a05605ee91950916
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-18 23:04:19 +0600

    minor changes

--- siam/index.php
+++ siam/index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JOB SITE 101</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
     <div class="bg">
         <h1 class="welcome">WELCOME</h1>
-        <form class="login_form" action="login_check.php" method="post">
+        <form class="login_form" action="login_check.php" method="POST">
             <div>
                 <label class="input_lable">USERNAME: </label>
                 <input class="login_input" type="text" name="username">
