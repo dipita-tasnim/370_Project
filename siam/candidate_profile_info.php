5aa63e1d65ce183fe2d7edfea5a819ae222cb8f7
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-19 12:38:46 +0600

    insert sql

---
+++ siam/candidate_profile_info.php
@@ -0,0 +1,23 @@
+<?php
+    require_once("connect.php");
+
+    if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) 
+        && isset($_POST['skill']) && isset($_POST['experience']) 
+            && isset($_POST['location'])){
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $phone = $_POST['phone'];
+                $skill = $_POST['skill'];
+                $experience = $_POST['experience'];
+                $location = $_POST['location'];
+                $sql = "INSERT INTO candidate VALUES('$candidate_id','$name', '$email', '$phone', '$skill', '$experience', '$location')";
+                $result = mysqli_query($conn, $sql);
+                if($result){
+                    header("Location: candidate_profile.php");
+                }
+                else {
+                    echo "Error: " . mysqli_error($conn);
+                }
+            }
+
+?>
