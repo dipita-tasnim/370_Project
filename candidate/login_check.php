61da6683f5aed4ac41610902587fd49dd097c8ce
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-23 04:30:37 +0600

    minor changes

--- candidate/login_check.php
+++ candidate/login_check.php
@@ -6,11 +6,11 @@
         $name = $_POST['name'];
         $password = $_POST['password'];
         if($_POST['candidate']){
-            $sql = "SELECT * FROM candidate WHERE name = '$name' AND password = '$password'";
-            $result = mysqli_query($conn, $sql);
-
-            if(mysqli_num_rows($result) > 0){
-                header("Location: homepage.php");
+            $res = mysqli_query($conn, "SELECT candidate_id FROM candidate WHERE name = '$name' AND password = '$password'");
+            if(mysqli_num_rows($res) > 0){
+                $row = mysqli_fetch_assoc($res);
+                $_SESSION['candidate_id'] = $row['candidate_id'];
+                header("Location: candidate_profile_display.php");
             }
             else{
                 echo "Wrong username or password";
@@ -19,10 +19,10 @@
         else{
             if($_POST['employer']){
                 $res = mysqli_query($conn, "SELECT company_id FROM company WHERE name = '$name' AND password = '$password'");
-                $row = mysqli_fetch_assoc($res);
-                $_SESSION['company_id'] = $row['company_id'];
                 if(mysqli_num_rows($res) > 0){
                     header("Location: ../company/company_profile.php");
+                    $row = mysqli_fetch_assoc($res);
+                    $_SESSION['company_id'] = $row['company_id'];
                 }
                 else{
                     echo "Wrong username or password";
