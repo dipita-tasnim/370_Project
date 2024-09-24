cd663e1e37a1a1c1b0d5bc84af1cc74eb7b8aacd
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-25 04:06:09 +0600

    displayes candidate info and applied job info

--- candidate/candidate_profile_display.php
+++ candidate/candidate_profile_display.php
@@ -17,6 +17,7 @@
             $sql = "SELECT * FROM candidate WHERE candidate_id = '$candidate_id'";
             $result = mysqli_query($conn, $sql);
             while($row = mysqli_fetch_array($result)){
+                $job_id = $row["job_id"];
         ?>
         <h1>Welcome <?php echo $row["name"]; ?></h1>
         <button class ="btn btn-primary" onclick="location.href='candidate_profile_edit.php'">Edit Profile</button>
@@ -26,27 +27,69 @@
         <table class="table">
             <thead>
                 <tr>
+                    <th>candidate_id</th>
                     <th>Email</th>
                     <th>Phone</th>
                     <th>Skills</th>
                     <th>Experience</th>
                     <th>Location</th>
+                    <th>job_id</th>
                 </tr>
             </thead>
             <tbody>
         
             <tr>
+                <td><?php echo $row["candidate_id"]; ?></td>
                 <td><?php echo $row["email"]; ?></td>
                 <td><?php echo $row["phone"]; ?></td>
                 <td><?php echo $row["skills"]; ?></td>
                 <td><?php echo $row["experience"]; ?></td>
                 <td><?php echo $row["location"]; ?></td>
+                <td><?php echo $row["job_id"]; ?></td>
             </tr>
         <?php
             }
             ?>
             </tbody>
         </table>
+        <h1>Jobs That You Applied</h1>
+        <div class="container my-5">
+            <?php
+                $sql = "SELECT * FROM job WHERE job_id = '$job_id'";
+                $result = mysqli_query($conn, $sql);
+                while($row = mysqli_fetch_array($result)){
+                    ?>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Job Title</th>
+                                <th>Job Description</th>
+                                <th>Job Location</th>
+                                <th>Required Skill</th>
+                                <th>Required Experience</th>
+                                <th>Salary</th>
+                                <th>Posted Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?php echo $row["title"]; ?></td>
+                                <td><?php echo $row["description"]; ?></td>
+                                <td><?php echo $row["location"]; ?></td>
+                                <td><?php echo $row["required_skill"]; ?></td>
+                                <td><?php echo $row["required_experience"]; ?></td>
+                                <td><?php echo $row["salary"]; ?></td>
+                                <td><?php echo $row["posted_date"]; ?></td>
+                                <td>CANCEL JOB</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <?php
+                    }
+                    ?>
+    </div>
+        <button class ="btn btn-primary" onclick="location.href='homepage.php'">Return To Homepage</button>
     </div>    
 </body>
 </html>
