9910bb6219a81623ac7e8eb7dd488c331a62fdd6
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-25 05:18:15 +0600

    minor change

--- candidate/index.php
+++ candidate/index.php
@@ -4,23 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JOB SITE 101</title>
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
     <div class="bg">
-        <h1 class="welcome">WELCOME</h1>
-        <form class="login_form" action="login_check.php" method="POST">
-            <input class="login_input" type="text" name="name" placeholder="Name">
+        <div class="login">
+            <h1 class="welcome">WELCOME</h1>
+            <form class="login_form" action="login_check.php" method="POST">
+                <input class="login_input" type="text" name="name" placeholder=" Name">
+                <br>
+                <input class="login_input" type="password" name="password" placeholder=" Password">
+                <br>
+                <h3>Choose only one option</h6>
+                <div class="checkbox">
+                    <input type="checkbox" name="candidate" >candidate
+                    <input type="checkbox" name="employer" >employer
+                </div>
+                <br>
+                <br>
+                <input class="login_button" type="submit" value="LOGIN" name="login">
+            </form>
             <br>
-            <input class="login_input" type="password" name="password" placeholder="Password">
-            <br>
-            <input type="checkbox" name="candidate">candidate
-            <input type="checkbox" name="employer">employer
-            <br>
-            <input type="submit" value="LOGIN" name="login">
-        </form>
-        <button class="create_acc_candidate" onclick="location.href='create_acc_candidate.php'">Create Account as a CANDIDATE</button> 
-        <button class="create_acc_employer" onclick="location.href='../company/company_create.php'">Create Account as an EMPLOYER</button>
+            <div class="create_acc">
+                <button class="create_acc_candidate" onclick="location.href='create_acc_candidate.php'">Create Account as a CANDIDATE</button>
+                <button class="create_acc_employer" onclick="location.href='../company/company_create.php'">Create Account as an EMPLOYER</button>
+            </div>
+        </div>
     </div>
 </body>
 </html>
