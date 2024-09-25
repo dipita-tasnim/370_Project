9a42ff59163b0e9e3a690ac5588e42da1f15bc56
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-25 17:34:12 +0600

    appy job

--- candidate/apply_job.php
+++ candidate/apply_job.php
@@ -3,19 +3,9 @@
     require_once("connect.php");
     $job_id = $_POST['job_id'];
     $candidate_id = $_SESSION['candidate_id'];
-    $sql_candidate_info = "SELECT * FROM candidate WHERE candidate_id = '$candidate_id'";
-    $res_can = mysqli_query($conn, $sql_candidate_info);
-    $row = mysqli_fetch_array($res_can);
-    $name = $row['name'];
-    $email = $row['email'];
-    $phone = $row['phone'];
-    $address = $row['address'];
-    $skills = $row['skills'];
-    $experience = $row['experience'];
-    $location = $row['location'];
     
-    $sql = "INSERT INTO candidate (name, email, phone, skills, experience, location, password)
-            VALUES('$name', '$email', '$phone', '$skills', '$experience', '$location', '$password')";
+    $sql = "INSERT INTO job_application (application_id, candidate_id, job_id)
+            VALUES('$application_id', '$candidate_id', '$job_id')";
     $result = mysqli_query($conn, $sql);
     if($result){
         header("Location: candidate_profile_display.php");
