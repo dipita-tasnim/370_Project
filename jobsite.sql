f7ee4350c3cd5f9ab19a698eaa46c0ad8f39d8d1
Sadman-Siam <sadman.bin.anwar.siam@gmail.com>
2024-09-25 15:53:29 +0600

    updated sql

--- jobsite.sql
+++ jobsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2024 at 01:17 AM
+-- Generation Time: Sep 25, 2024 at 11:52 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,18 +35,17 @@ CREATE TABLE `candidate` (
   `skills` varchar(200) NOT NULL,
   `experience` varchar(200) NOT NULL,
   `location` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `job_id` int(5) DEFAULT NULL
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `candidate`
 --
 
-INSERT INTO `candidate` (`candidate_id`, `name`, `email`, `phone`, `skills`, `experience`, `location`, `password`, `job_id`) VALUES
-(10, 'Siam', 'siam@gmail.com', 1356453432, 'Python, Java, PHP, SQL', 'none', 'Dhaka', 'siam1234', 7),
-(11, 'sinka', 'sinka@gmail.com', 1898765643, 'Python', 'none', 'Dhaka', 'sinka1234', NULL),
-(13, 'aa', 'aa', 11, 'aa', 'aa', 'aa', 'aa', 8);
+INSERT INTO `candidate` (`candidate_id`, `name`, `email`, `phone`, `skills`, `experience`, `location`, `password`) VALUES
+(10, 'Siam', 'siam@gmail.com', 1356453432, 'Python, Java, PHP, SQL', 'none', 'Dhaka', 'siam1234'),
+(11, 'sinka', 'sinka@gmail.com', 1898765643, 'Python', 'none', 'Dhaka', 'sinka1234'),
+(13, 'aa', 'aa', 11, 'aa', 'aa', 'aa', 'aa');
 
 -- --------------------------------------------------------
 
@@ -69,9 +68,8 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`company_id`, `name`, `company`, `email`, `industry`, `location`, `password`) VALUES
-(7, 'Siam', 'Thryve', 'thryve@gmail.com', 'clothing', 'Dhaka', 'thryve1234'),
-(8, 'Dipita', 'Arong', 'arong@gmail.com', 'clothing', 'Dhaka', 'arong1234'),
-(9, 'Angon', 'prime', 'Prime@gmail.com', 'manufacturing', 'USA', 'prime1234');
+(2, 'Siam', 'Thryve', 'Thryve@gmail.com', 'manufacturing', 'Dhaka', '1234'),
+(3, 'dipita', 'Arong', 'arong@gmail.com', 'manufacturing', 'Dhaka', '1234');
 
 -- --------------------------------------------------------
 
@@ -81,7 +79,7 @@ INSERT INTO `company` (`company_id`, `name`, `company`, `email`, `industry`, `lo
 
 CREATE TABLE `job` (
   `job_id` int(5) NOT NULL,
-  `company_id` int(5) NOT NULL,
+  `company_id` int(5) DEFAULT NULL,
   `title` varchar(50) NOT NULL,
   `description` varchar(1000) NOT NULL,
   `location` varchar(50) NOT NULL,
@@ -96,8 +94,10 @@ CREATE TABLE `job` (
 --
 
 INSERT INTO `job` (`job_id`, `company_id`, `title`, `description`, `location`, `required_skill`, `required_experience`, `salary`, `posted_date`) VALUES
-(7, 9, 'it', 'gg', 'Dhaka', 'none', 'none', 200, '2024-08-12'),
-(8, 9, 'ok', 'ok', 'ok', 'ok', 'ok', 0, '2024-09-01');
+(8, 2, 'ok', 'ok', 'ok', 'ok', 'ok', 0, '2024-09-01'),
+(9, 2, 'modarator', 'no money only love', 'Dhaka', 'none', 'none', 500, '2024-01-01'),
+(10, 2, 'It assistant', 'A very courageous person to handle daily It related complains ', 'Rajshahi', 'none', 'none', 1000, '2024-02-02'),
+(11, 3, 'guard', 'protect ', 'Dhaka', 'strong', '5 years', 5, '2024-03-03');
 
 -- --------------------------------------------------------
 
@@ -108,10 +108,7 @@ INSERT INTO `job` (`job_id`, `company_id`, `title`, `description`, `location`, `
 CREATE TABLE `job_application` (
   `application_id` int(5) NOT NULL,
   `candidate_id` int(5) NOT NULL,
-  `company_id` int(5) NOT NULL,
-  `job_id` int(5) NOT NULL,
-  `application_date` date NOT NULL,
-  `status` varchar(20) NOT NULL
+  `job_id` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -122,8 +119,7 @@ CREATE TABLE `job_application` (
 -- Indexes for table `candidate`
 --
 ALTER TABLE `candidate`
-  ADD PRIMARY KEY (`candidate_id`),
-  ADD KEY `job_id` (`job_id`);
+  ADD PRIMARY KEY (`candidate_id`);
 
 --
 -- Indexes for table `company`
@@ -136,7 +132,7 @@ ALTER TABLE `company`
 --
 ALTER TABLE `job`
   ADD PRIMARY KEY (`job_id`),
-  ADD KEY `company_id_fk` (`company_id`);
+  ADD KEY `company_id` (`company_id`);
 
 --
 -- Indexes for table `job_application`
@@ -144,7 +140,6 @@ ALTER TABLE `job`
 ALTER TABLE `job_application`
   ADD PRIMARY KEY (`application_id`),
   ADD KEY `candidate_id_fk` (`candidate_id`),
-  ADD KEY `company_id_fk` (`company_id`),
   ADD KEY `job_id_fk` (`job_id`);
 
 --
@@ -155,19 +150,19 @@ ALTER TABLE `job_application`
 -- AUTO_INCREMENT for table `candidate`
 --
 ALTER TABLE `candidate`
-  MODIFY `candidate_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `candidate_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `company_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `company_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `job`
 --
 ALTER TABLE `job`
-  MODIFY `job_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `job_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `job_application`
@@ -180,12 +175,6 @@ ALTER TABLE `job_application`
 --
 
 --
--- Constraints for table `candidate`
---
-ALTER TABLE `candidate`
-  ADD CONSTRAINT `candidate_ibfk_1` FOREIGN KEY (`job_id`) REFERENCES `job` (`job_id`);
-
---
 -- Constraints for table `job`
 --
 ALTER TABLE `job`
@@ -196,7 +185,6 @@ ALTER TABLE `job`
 --
 ALTER TABLE `job_application`
   ADD CONSTRAINT `job_application_ibfk_1` FOREIGN KEY (`candidate_id`) REFERENCES `candidate` (`candidate_id`),
-  ADD CONSTRAINT `job_application_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`),
   ADD CONSTRAINT `job_application_ibfk_3` FOREIGN KEY (`job_id`) REFERENCES `job` (`job_id`);
 COMMIT;
 
