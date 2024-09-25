bf889c24b285597abd72506862e3b5f9a51fc45f
Sadman-Shakib-Aungon <sadmanshakibaungon@gmail.com>
2024-09-25 12:16:12 +0600

    add

--- jobdescription.php
+++ jobdescription.php
@@ -1,28 +1,45 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "jobsite";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT job_id,title,location,required_skill,required_experience,salary,posted_date * FROM job"; // getting the info from job table
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<h2>" . $row["title"] . "</h2>";
-        echo "<p>" . $row["description"] . "</p>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
+    session_start();
+    require_once("connection.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="jobdescription.css">
+    <title>Job Description</title>
+</head>
+<body>
+    <div class="container my-5">
+    <?php
+        $job_id = $_GET['job_id'];  // Assume job_id is passed via URL
+        $sql = "SELECT * FROM job WHERE job_id = '$job_id'";
+        $result = mysqli_query($conn, $sql);
+        if ($row = mysqli_fetch_array($result)) {
+    ?>
+        <h1>Job Title: <?php echo $row["title"]; ?></h1>
+        <h5>Job Information</h5>
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>Job ID <?php echo $row["job_id"]; ?> </td>
+                    <td>Description <?php echo $row["description"]; ?></td>
+                    <td>Location <?php echo $row["location"]; ?></td>
+                    <td>Required Skill <?php echo $row["required_skill"]; ?></td>
+                    <td>Required Experience<?php echo $row["required_experience"]; ?></td>
+                    <td>Salary <?php echo $row["salary"]; ?></td>
+                    <td>Posted Date <?php echo $row["posted_date"]; ?></td>
+                </tr>
+            </thead>
+
+        </table>
+        
+    <?php
+        } else {
+            echo "<h3>Job not found!</h3>";
+        }
+    ?>
+    </div>    
+</body>
+</html>
